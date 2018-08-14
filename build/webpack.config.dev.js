b9b546dfe2e9a7eab752d81fd2073b26e5bb7a69
minco <minocoko@outlook.com>
2018-08-14 23:09:06 +0800

    add webpack clean & html plugins

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -1,8 +1,11 @@
 const path = require("path");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+// const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ScriptExtHtmlWebpackPlugin = require("script-ext-html-webpack-plugin");
 const outputDirectory = path.join(__dirname, "../dist");
 const bundelEsModules = process.env.BUNDEL_ES_MODULES === "true";
-const version = require(path.join(__dirname,"../package.json")).version;
+const version = require(path.join(__dirname, "../package.json")).version;
 
 const presets = [];
 
@@ -31,9 +34,11 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: outputDirectory,
-    filename: `js/main.v${version}.${bundelEsModules ? "mjs" : "js"}`,
-    chunkFilename: `js/[name].main.v${version}.${bundelEsModules ? "mjs" : "js"}`,
-    // publicPath: "public"
+    filename: `js/main.v${version}-[hash:8].${bundelEsModules ? "mjs" : "js"}`,
+    chunkFilename: `js/[name].main.v${version}-[hash:8].${
+      bundelEsModules ? "mjs" : "js"
+    }`
+    // publicPath: 'public'
   },
   module: {
     rules: [
@@ -51,8 +56,25 @@ module.exports = {
     ]
   },
   plugins: [
-    new CopyWebpackPlugin([
-      { from: "./public/*", to: outputDirectory, flatten: true }
-    ])
+    new CleanWebpackPlugin(["dist"], { root: path.join(__dirname, "../") }),
+    new HtmlWebpackPlugin({
+      title: "demo",
+      meta: {
+        viewport: "width=device-width, initial-scale=1, shrink-to-fit=no",
+        "http-equiv": "X-UA-Compatible",
+        content: "IE=edge"
+      }
+    }),
+    new ScriptExtHtmlWebpackPlugin({
+      // custom: [{
+      //   test: /main.*\.js$/,
+      //   attribute: 'type',
+      //   value: 'text/javascript'
+      // }],
+      module: /main.*\.mjs/
+    })
+    // new CopyWebpackPlugin([
+    //   { from: "./public/*", to: outputDirectory, flatten: true }
+    // ])
   ]
 };
